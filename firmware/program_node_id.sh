49ba8b6c6fc51538cd9373fc49e02d54e044e329
Robert Kaye <rob@musicbrainz.org>
2015-04-18 09:49:16 -0700

    Blah

--- firmware/program_node_id.sh
+++ firmware/program_node_id.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 ./generate_node_id.py id.raw $1
-sudo avrdude -p m328p -P usb -c avrispmkII -U eeprom:w:id.raw:r -B 1.0
+sudo avrdude -p m328p -P usb -c avrispmkII -U eeprom:w:id.raw:r
 rm -f id.raw
