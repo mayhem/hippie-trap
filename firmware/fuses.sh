379c60a3c203a3d4e526cea9d96057cec3889fbf
Robert Kaye <rob@musicbrainz.org>
2015-06-30 15:27:47 +0200

    No longer needed

--- firmware/fuses.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-#avrdude -p m328p -P usb -c avrispmkII -U lfuse:w:0xe2:m -U hfuse:w:0xD1:m -U efuse:w:0xff:m -B 8
-sudo avrdude -p m328p -P usb -c avrispmkII -U lfuse:w:0xe2:m -U hfuse:w:0xd1:m -U efuse:w:0xff:m
