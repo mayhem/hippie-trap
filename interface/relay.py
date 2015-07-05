a63eb5a301d4fe4423151f14accb252996acefe3
Robert Kaye <rob@musicbrainz.org>
2015-07-05 12:56:48 +0200

    Finished debugging the relay

--- interface/relay.py
+++ interface/relay.py
@@ -3,11 +3,13 @@
 import os
 import sys
 import serial
+import urllib2
 from time import sleep, time
 
 BAUD_RATE = 9600
+#IP = "127.0.0.1"
 IP = "10.0.0.1"
-PORT = "8080"
+PORT = 8080
 
 class Relay(object):
 
@@ -18,15 +20,16 @@ class Relay(object):
                                      BAUD_RATE, 
                                      bytesize=serial.EIGHTBITS, 
                                      parity=serial.PARITY_NONE, 
-                                     stopbits=serial.STOPBITS_ONE,
-                                     timeout=.01)
+                                     stopbits=serial.STOPBITS_ONE)
         except serial.serialutil.SerialException, e:
             print "Cant open serial port: %s" % device
             sys.exit(-1)
 
     def run(self):
         while True:
-            ch = self.ser.read()
+            ch = self.ser.read(1)
+            if not ch:
+                continue
             if ch < "0" or ch > "5":
                 continue
 
@@ -36,18 +39,23 @@ class Relay(object):
             elif ch == "1":
                 arg = "bdown"
             elif ch == "2":
-                arg == "sdown"
+                arg = "sdown"
             elif ch == "3":
                 arg = "bup"
             elif ch == "4":
-                arg == "hold"
+                arg = "hold"
             elif ch == "5":
-                arg == "sup"
+                arg = "sup"
 
             if arg:
-                url = "http://%s:%d/%s" % (IP, PORT, arg)
-                print url
-                urllib.open(url)
+                print arg
+                url = "http://%s:%d/ctrl/%s" % (IP, PORT, arg)
+                try:
+                    urllib2.urlopen(url)
+                except urllib2.URLError:
+                    pass
+                except urllib2.HTTPError:
+                    pass
 
 if __name__ == "__main__":
     r = Relay()
