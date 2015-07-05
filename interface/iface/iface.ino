aad9a2dc747060e72eefcb69d09596e3e8e7e642
Robert Kaye <rob@musicbrainz.org>
2015-07-05 11:13:37 +0200

    Set up for 6 buttons and recorded mapping

--- interface/iface/iface.ino
+++ interface/iface/iface.ino
@@ -1,6 +1,6 @@
 #include <CapacitiveSensor.h>
 
-#define MAX_SENSORS 9
+#define MAX_SENSORS 6
 
 CapacitiveSensor cs0 = CapacitiveSensor(2,3);
 CapacitiveSensor cs1 = CapacitiveSensor(4,5);
@@ -8,12 +8,12 @@ CapacitiveSensor cs2 = CapacitiveSensor(6,7);
 CapacitiveSensor cs3 = CapacitiveSensor(8,9);
 CapacitiveSensor cs4 = CapacitiveSensor(10,11);
 CapacitiveSensor cs5 = CapacitiveSensor(12,13);
-CapacitiveSensor cs6 = CapacitiveSensor(14,15);
-CapacitiveSensor cs7 = CapacitiveSensor(16,17);
-CapacitiveSensor cs8 = CapacitiveSensor(18,19);
+//CapacitiveSensor cs6 = CapacitiveSensor(14,15);
+//CapacitiveSensor cs7 = CapacitiveSensor(16,17);
+//CapacitiveSensor cs8 = CapacitiveSensor(18,19);
 
-const long threshold = 300;
-const long num = 9;
+const long threshold = 700;
+const long num = 6;
 long states[MAX_SENSORS];
 
 void setup() 
@@ -31,9 +31,9 @@ void setup()
     cs3.set_CS_AutocaL_Millis(0xFFFFFFFF);
     cs4.set_CS_AutocaL_Millis(0xFFFFFFFF);
     cs5.set_CS_AutocaL_Millis(0xFFFFFFFF);
-    cs6.set_CS_AutocaL_Millis(0xFFFFFFFF);
-    cs7.set_CS_AutocaL_Millis(0xFFFFFFFF);
-    cs8.set_CS_AutocaL_Millis(0xFFFFFFFF);
+    //cs6.set_CS_AutocaL_Millis(0xFFFFFFFF);
+    //cs7.set_CS_AutocaL_Millis(0xFFFFFFFF);
+    //cs8.set_CS_AutocaL_Millis(0xFFFFFFFF);
 }
 
 void check_sensor(uint8_t id, CapacitiveSensor *s)
@@ -41,11 +41,11 @@ void check_sensor(uint8_t id, CapacitiveSensor *s)
     long total;
         
     total = s->capacitiveSensor(30);
-    Serial.println(total);
+    //Serial.println(total);
         
     if (total > threshold && states[id] == 0)
     {
-//        Serial.println(id);
+        Serial.println(id);
         states[id] = 1;
     }
     if (total <= threshold && states[id] == 1)
@@ -58,8 +58,7 @@ void loop()
     long total;
     
     delay(100);
-
-#if 0    
+    
     check_sensor(0, &cs0);
     if (num == 1)
         return;
@@ -83,16 +82,18 @@ void loop()
     check_sensor(5, &cs5);
     if (num == 6)
         return;     
-
+#if 0
     check_sensor(6, &cs6);
+
+    
     if (num == 7)
         return;
 
     check_sensor(7, &cs7);
     if (num == 8)
         return; 
-#endif
         
     check_sensor(8, &cs8);
+#endif
 }
 
