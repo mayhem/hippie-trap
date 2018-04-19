b91f1d7ea6201fdaedbb00fb2e837626734a23ea
Robert Kaye <rob@musicbrainz.org>
2018-04-19 21:52:43 +0000

    Add the panic recovery script

---
+++ software/bin/panic.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import os
+import sys
+import math
+from colorsys import hsv_to_rgb
+from hippietrap.chandelier import Chandelier, BROADCAST, NUM_NODES
+from hippietrap.color import Color
+from time import sleep, time
+
+
+device = "/dev/serial0"
+
+ch = Chandelier()
+ch.open(device)
+ch.send_panic()
