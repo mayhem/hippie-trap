0e174fb4033f3b8720728d302fcb3ad6f9b2ee0e
Robert Kaye <rob@musicbrainz.org>
2018-05-14 23:12:47 +0000

    Toss out the brightness stuff.

--- software/dev/rainbow.py
+++ software/dev/rainbow.py
@@ -12,14 +12,10 @@ STEPS = 5000
 
 with HippieTrap() as ch:
     try:
-        t = 0
         while True:
             for i in range(STEPS):
                 rgb = hsv_to_rgb(i / float(STEPS), 1.0, 1.0)
                 ch.set_color(BROADCAST, Color(int(255 * rgb[0]), int(255 * rgb[1]), int(255 * rgb[2])))
-                print (t / 4) % 100
-                ch.set_brightness(BROADCAST, (t / 4) % 100)
-                t += 1
                 sleep(.02)
     except KeyboardInterrupt:
         ch.clear_cruft()
