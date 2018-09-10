4ab78152e5e5e027a21740599aac3826c2152700
Robert Kaye <rob@musicbrainz.org>
2018-09-11 00:52:23 +0200

    Fixing up patterns that relied on being two rings.

--- software/patterns/fire_ice_circles.py
+++ software/patterns/fire_ice_circles.py
@@ -4,7 +4,7 @@ import os
 import sys
 import math
 from colorsys import hsv_to_rgb
-from hippietrap.hippietrap import HippieTrap, NUM_NODES, ALL
+from hippietrap.hippietrap import HippieTrap, NUM_NODES, ALL, BOTTLES_PER_RING
 from hippietrap.color import hue_to_color
 from hippietrap.pattern import PatternBase, run_pattern
 from time import sleep, time
@@ -14,19 +14,23 @@ from hippietrap.transition import transition_sweep_out
 class Pattern(PatternBase):
 
     PERIOD = 1500
-    angle = .02
+    angle = .08
     name = "fire ice circles"
 
     def pattern(self):
 
         color_offset = 0.0
         stop = False
-        while not stop:
-            for bottle in range(1, 14):
+
+        bottle_base = 1
+        for i, ring in enumerate(BOTTLES_PER_RING):
+            if i == len(BOTTLES_PER_RING):
+                break
+
+            for bottle in range(bottle_base, bottle_base + BOTTLES_PER_RING[i] - 1):
                 array = []
 
-                # refactor this copypasta!
-                hue = (random() / 5) + color_offset
+                hue = (random() / 5) + color_offset + (i * .5)
                 array.append(hue_to_color(min(1.0, math.fmod(hue, 1.0))))
                 array.append(hue_to_color(min(1.0, math.fmod(hue + self.angle, 1.0))))
                 array.append(hue_to_color(min(1.0, math.fmod(hue + (self.angle * 2), 1.0))))
@@ -41,24 +45,7 @@ class Pattern(PatternBase):
                     stop = True
                     break
 
-            for bottle in range(14, 31):
-                array = []
-
-                hue = (random() / 5) + color_offset + .5
-                array.append(hue_to_color(min(1.0, math.fmod(hue, 1.0))))
-                array.append(hue_to_color(min(1.0, math.fmod(hue + self.angle, 1.0))))
-                array.append(hue_to_color(min(1.0, math.fmod(hue + (self.angle * 2), 1.0))))
-                array.append(hue_to_color(min(1.0, math.fmod(hue + (self.angle * 3), 1.0))))
-
-                self.trap.stop_pattern(bottle)
-                self.trap.send_fade(bottle, self.PERIOD, array)
-                sleep(.035)
-                self.trap.start_pattern(bottle)
-            
-                if self.stop_thread:
-                    stop = True
-                    break
-
+            bottle_base += BOTTLES_PER_RING[i]
             color_offset += .2
 
         self.trap.stop_pattern(ALL)
--- software/patterns/sweep_two_color_shift.py
+++ software/patterns/sweep_two_color_shift.py
@@ -6,7 +6,7 @@ import math
 from colorsys import hsv_to_rgb
 from random import random
 from hippietrap.hippietrap import HippieTrap, ALL, NUM_NODES
-from hippietrap.color import Color, ColorGenerator
+from hippietrap.color import Color, ColorGenerator, random_color, hue_to_color
 from hippietrap.geometry import HippieTrapGeometry
 from hippietrap.pattern import PatternBase, run_pattern
 from time import sleep, time
@@ -26,20 +26,22 @@ class Pattern(PatternBase):
         index = 0
         hue_offset = 0.0
         stop = False
+
+        color_rings = [ random_color(), random_color(), random_color() ]
         while not stop:
-            col1, col2 = self.cg.random_color_pair(hue_offset)
             for bottle, angle in self.geo.enumerate_all_bottles(index % 2 == 0):
-                if self.geo.get_ring_from_bottle(bottle) % 2 == 0:
-                    self.trap.set_color(bottle, col1)
-                else:
-                    self.trap.set_color(bottle, col2)
+                self.trap.set_color(bottle, color_rings[self.geo.get_ring_from_bottle(bottle)])
                 sleep(.02)
                 if self.stop_thread:
                     stop = True
                     break
 
             index += 1
-            hue_offset = math.fmod(hue_offset + .01, 1.0)
+            hue_offset = math.fmod(hue_offset + .1, 1.0)
+            shift = math.sin(index / .5) / 2.0 + .50
+            new_offset = math.fmod(shift, 1.0)
+            color_rings.pop()
+            color_rings.insert(0, hue_to_color(new_offset))
 
         self.trap.stop_pattern(ALL)
         if self.transition:
