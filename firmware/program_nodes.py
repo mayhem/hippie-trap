1481c325084f327a1f55dd29fe526a1dd96429b8
Robert Kaye <rob@musicbrainz.org>
2018-09-11 14:06:50 +0000

    Added delays and better error checking to node programmer

--- firmware/program_nodes.py
+++ firmware/program_nodes.py
@@ -10,10 +10,11 @@ import subprocess
 import random
 import argparse
 import errno
+from time import sleep
 
 NODE_ID_FILE = "/tmp/node.raw"
 BOOT_CALL = ["make", "-C", "bootloader"]
-FUSES_CALL = ["make", "-C", "bootloader"]
+FUSES_CALL = ["make", "-C", "fuses"]
 ID_CALL = ["sudo", "avrdude", "-p", "m328p", "-P", "usb", "-c", "avrispmkII", "-U", "eeprom:w:" + NODE_ID_FILE + ":r"]
 
 def write_node_id(file_name, id):
@@ -28,30 +29,41 @@ def write_node_id(file_name, id):
 
 parser = argparse.ArgumentParser()
 parser.add_argument("id", help="The next node id to use", type=int, default=1)
+parser.add_argument("delay", help="Delay between progamming nodes", type=int, default=0)
 args = parser.parse_args()
 
 id = args.id
+delay = args.delay
 
 while True:
+    if delay:
+        print "Waiting %s seconds..." % delay
+        sleep(delay)
+    else:
+        print "READY FOR NEXT NODE -- Press key to program"
+        raw_input("")
+
     print "programming bootloader: *******************************"
     try:
         subprocess.check_call(BOOT_CALL, stderr=subprocess.STDOUT)
     except subprocess.CalledProcessError as e:
-        pass
+        print "PROGRAMMING FAILED!!! DO NOT MOVE TO NEXT NODE!"
+        continue
 
     print "programming fuses: *******************************"
     try:
         subprocess.check_call(FUSES_CALL, stderr=subprocess.STDOUT)
     except subprocess.CalledProcessError as e:
-        pass
+        print "PROGRAMMING FAILED!!! DO NOT MOVE TO NEXT NODE!"
+        continue
 
     print "writing node id: *********************************"
     write_node_id(NODE_ID_FILE, id)
     try:
         subprocess.check_output(ID_CALL)
     except subprocess.CalledProcessError as e:
-        pass
+        print "PROGRAMMING FAILED!!! DO NOT MOVE TO NEXT NODE!"
+        continue
 
-    print "Node id %d programmed. Press key for next node." % id
-    raw_input("")
+    print "Node id %d programmed." % id
     id += 1
