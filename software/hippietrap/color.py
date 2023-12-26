bb3d1b02f076f2088e85f4abd2190d96a4209e08
Robert Kaye <rob@musicbrainz.org>
2023-12-26 18:44:46 +0100

    Check in latest improvements before OS upgrade

--- software/hippietrap/color.py
+++ software/hippietrap/color.py
@@ -105,9 +105,64 @@ class ColorGenerator(object):
 
         return (Color(int(255 * rgb[0]), int(255 * rgb[1]), int(255 * rgb[2])), 
                 Color(int(255 * rgb2[0]), int(255 * rgb2[1]), int(255 * rgb2[2])))
+from colorsys import hsv_to_rgb
+from math import fabs, fmod
+import os
+from neopixel import Color
 
-if __name__ == "__main__":
-    c1 = Color(1,1,128)
-    c2 = Color(2,2,128)
-    c1 += c2
-    print(c1.red, c1.green, c1.blue)
+class Gradient(object):
+
+    def __init__(self, palette, num_leds = 1):
+
+        # palletes are in format [ (.345, (128, 0, 128)) ]
+        self._validate_palette(palette)
+        self.palette = palette
+        self.num_leds = num_leds
+        self.led_scale = 1.0
+        self.led_offset = 0.0
+
+
+    def _validate_palette(self, palette):
+
+        if len(palette) < 2:
+            raise ValueError("Palette must have at least two points.")
+
+        if palette[0][0] > 0.0:
+            raise ValueError("First point in palette must be less than or equal to 0.0")
+
+        if palette[-1][0] < 1.0:
+            raise ValueError("Last point in palette must be greater than or equal to 1.0")
+
+
+    def set_scale(self, scale):
+        self.led_scale = scale
+
+
+    def set_offset(self, offset):
+        self.led_offset = offset
+
+
+    def get_color_by_offset(self, offset):
+
+        if offset < 0.0 or offset > 1.0:
+            raise IndexError("Invalid offset.")
+
+        for index in range(len(self.palette)):
+
+            # skip the first item
+            if index == 0:
+                continue
+
+            if self.palette[index][0] >= offset:
+                section_begin_offset = self.palette[index-1][0]
+                section_end_offset = self.palette[index][0]
+
+                percent = (offset - section_begin_offset) / (section_end_offset - section_begin_offset)
+                new_color = []
+                for color in range(3):
+                    new_color.append(int(self.palette[index-1][1][color] + 
+                            ((self.palette[index][1][color] - self.palette[index-1][1][color]) * percent)))
+
+                return (min(new_color[0], 255), min(new_color[1], 255), min(new_color[2], 255))
+
+        assert False
---
+++ software/patterns/sparkle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import math
+from random import random, randint
+from hippietrap.hippietrap import HippieTrap, ALL, NUM_NODES
+from hippietrap.pattern import PatternBase, run_pattern
+from hippietrap.color import hue_to_color, random_color
+from time import sleep, time
+
+class SparklePattern(PatternBase):
+
+    name = "sparkle"
+    PERIOD = 1000
+
+    def pattern(self):
+
+        stop = False
+        while not stop:
+            bottle = randint(1, NUM_NODES)
+            self.trap.set_color(bottle, random_color())
+            sleep(.01)
+            self.trap.send_fade(bottle, self.PERIOD, [(0,0,0),(0,0,0),(0,0,0),(0,0,0)])
+            sleep(.01)
+            self.trap.start_pattern(bottle)
+            sleep(.01)
+
+            if self.stop_thread:
+                stop = True
+                break
+
+        self.trap.stop_pattern(ALL)
